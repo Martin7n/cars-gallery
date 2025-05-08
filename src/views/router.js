4a823012cae5ae43718d09c2be6ecb1957cadb43
Martin7n <122726440+Martin7n@users.noreply.github.com>
2025-05-08 19:26:22 +0300

    basic visual structure and routing

---
+++ src/views/router.js
@@ -0,0 +1,19 @@
+import page from 'page';
+import homeRender from './home.js';
+import renderNav from './navigation.js';
+import galleryRender from './gallery.js'
+import loginRender from './login.js'
+import register from './register.js'
+
+
+
+export const setupRoutes = () => {
+    page(renderNav);
+    page('/', homeRender);
+    page('/gallery', galleryRender);
+    page('/login', loginRender);
+    page('/register', register)
+
+  
+    page();
+  };
